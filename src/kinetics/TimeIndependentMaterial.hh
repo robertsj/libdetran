fd55a3344490a2ea92130f3de4f4d9307cc244c6
Relzohery <rabab.elzohery@gmail.com>
2020-03-30 10:55:40 -0500

    added TimeIndependentMaterial class

---
+++ src/kinetics/TimeIndependentMaterial.hh
@@ -0,0 +1,62 @@
+//----------------------------------*-C++-*----------------------------------//
+/**
+ *  @file   TimeIndependentMaterial.hh
+ *  @author Rabab Elzohery
+ *  @date   March 28, 2020
+ *  @brief  TimeIndependentMaterial class definition.
+ */
+//---------------------------------------------------------------------------//
+
+#ifndef detran_TIMEINDEPENDENTMATERIAL_HH_
+#define detran_TIMEINDEPENDENTMATERIAL_HH_
+
+#include "TimeDependentMaterial.hh"
+#include "utilities/Definitions.hh"
+#include "utilities/SP.hh"
+
+namespace detran
+{
+
+/**
+ *  @class TimeIndependentMaterial
+ *  @brief Base class for time-Independent materials
+ */
+class KINETICS_EXPORT TimeIndependentMaterial: public TimeDependentMaterial
+{
+
+public:
+
+  //-------------------------------------------------------------------------//
+  // TYPEDEFS
+  //-------------------------------------------------------------------------//
+
+  typedef TimeDependentMaterial                             Base;
+  typedef Base::SP_material                                 SP_base;
+  typedef detran_utilities::SP<TimeIndependentMaterial>     SP_material;
+
+  //-------------------------------------------------------------------------//
+  // CONSTRUCTOR & DESTRUCTOR
+  //-------------------------------------------------------------------------//
+
+  /**
+   *  @brief Constructor
+   *  @param d_km          Referene of KineticsMaterial
+   */
+  TimeIndependentMaterial(KineticsMaterial::SP_material km);
+
+  static SP_material Create(KineticsMaterial::SP_material km);
+
+  KineticsMaterial ::SP_material d_km;
+
+  void update_impl();
+
+  /// Virtual destructor
+  virtual ~TimeIndependentMaterial(){}
+
+};
+
+KINETICS_TEMPLATE_EXPORT(detran_utilities::SP<TimeIndependentMaterial>)
+
+} // end namespace detran
+
+#endif /* detran_TIMEDEPENDENTMATERIAL_HH_ */
