184bdfa23d1a44b53d9e8218d807172f7deeaf60
Relzohery <rabab.elzohery@gmail.com>
2020-07-25 12:17:51 -0500

    extended to assembly

--- src/solvers/test/projection_fixture.hh
+++ src/solvers/test/projection_fixture.hh
@@ -48,7 +48,9 @@ Material::SP_material get_mat()
   mat->set_sigma_s(0, 0, 1, 0.0000);
   mat->set_sigma_s(0, 1, 0, 0.0380);
   mat->set_sigma_s(0, 1, 1, 1.4536);
-  mat->compute_diff_coef();                   ;
+  mat->compute_sigma_a();
+  mat->compute_diff_coef();
+
 
   // Material 1: Fuel I
   // Total
@@ -66,6 +68,7 @@ Material::SP_material get_mat()
   mat->set_sigma_s(1, 0, 1, 0.0000);
   mat->set_sigma_s(1, 1, 0, 0.0161);
   mat->set_sigma_s(1, 1, 1, 0.9355);
+  mat->compute_sigma_a();
   mat->compute_diff_coef();                   ;
 
 
@@ -86,8 +89,8 @@ Material::SP_material get_mat()
   mat->set_sigma_s(2, 0, 1, 0.0000);
   mat->set_sigma_s(2, 1, 0, 0.0156);
   mat->set_sigma_s(2, 1, 1, 0.9014);
+  mat->compute_sigma_a();
   mat->compute_diff_coef();                   ;
-
 
   // Material 4: Fuel II + Gd
 
@@ -106,16 +109,15 @@ Material::SP_material get_mat()
   mat->set_sigma_s(3, 0, 1, 0.0000);
   mat->set_sigma_s(3, 1, 0, 0.0136);
   mat->set_sigma_s(3, 1, 1, 0.5733);
+  mat->compute_sigma_a();
   mat->compute_diff_coef();                   ;
-
-
 
   mat->finalize();
 
   return mat;
 }
 
-Mesh1D::SP_mesh get_mesh(int fmm = 1)
+Mesh1D::SP_mesh get_mesh(int fmm = 1, std::string id="assembly")
 {
   vec_dbl cm_assembly(7, 0.0);
   cm_assembly[0] = 0.0;
@@ -126,10 +128,6 @@ Mesh1D::SP_mesh get_mesh(int fmm = 1)
   cm_assembly[5] = 14.4420;
   cm_assembly[6] = 15.6000;
 
-  std::vector<double> cm_core;
-
-  std::vector<double> fm_core();
-
   vec_int fm_assembly(6, 0);
 
   fm_assembly[0] = 2;
@@ -138,6 +136,20 @@ Mesh1D::SP_mesh get_mesh(int fmm = 1)
   fm_assembly[3] = 4;
   fm_assembly[4] = 4;
   fm_assembly[5] = 2;
+
+  // a core is composed of 7 adjacent assemblies
+  vec_dbl cm_core (43, 0);
+  vec_int fm_core(7*6);
+
+  for (int i=0; i <7; i++)
+   for (int j=0; j<6; j++)
+	 {
+
+      cm_core[j+(6*i) + 1] = cm_assembly[j +1] + 15.6 *i;
+
+	  fm_core[j+(6*i)] = fm_assembly[j];
+	 }
+  cm_core [0] = 0.0;
 
   //cm_core.assign(0.0 ,0.0);
 
@@ -161,9 +173,47 @@ Mesh1D::SP_mesh get_mesh(int fmm = 1)
   mt_3[2] = 3; mt_3[3] = 3;
   mt_3[4] = 3; mt_3[5] = 0;
 
-  Mesh1D::SP_mesh mesh = Mesh1D::Create(fm_assembly, cm_assembly, mt_0);
+  Mesh1D::vec_int mt_core0(42);
+  mt_core0[0] = 0; mt_core0[1] = 1;
+  mt_core0[2] = 2; mt_core0[3] = 2;
+  mt_core0[4] = 1; mt_core0[5] = 0;
 
+  mt_core0[6] = 0; mt_core0[7] = 1;
+  mt_core0[8] = 1; mt_core0[9] = 1;
+  mt_core0[10] = 1; mt_core0[11] = 0;
+
+  mt_core0[12] = 0; mt_core0[13] = 1;
+  mt_core0[14] = 2; mt_core0[15] = 2;
+  mt_core0[16] = 1; mt_core0[17] = 0;
+
+  mt_core0[18] = 0; mt_core0[19] = 1;
+  mt_core0[20] = 1; mt_core0[21] = 1;
+  mt_core0[22] = 1; mt_core0[23] = 0;
+
+  mt_core0[24] = 0; mt_core0[25] = 1;
+  mt_core0[26] = 2; mt_core0[27] = 2;
+  mt_core0[28] = 1; mt_core0[29] = 0;
+
+  mt_core0[30] = 0; mt_core0[31] = 1;
+  mt_core0[32] = 1; mt_core0[33] = 1;
+  mt_core0[34] = 1; mt_core0[35] = 0;
+
+  mt_core0[36] = 0; mt_core0[37] = 1;
+  mt_core0[38] = 2; mt_core0[39] = 2;
+  mt_core0[40] = 1; mt_core0[41] = 0;
+
+  if (id == "assembly")
+  {
+  Mesh1D::SP_mesh mesh = Mesh1D::Create(fm_assembly, cm_assembly, mt_0);
   return mesh;
+  }
+  else if (id == "core")
+  {
+   std::cout << " core mesh" << "\n";
+   Mesh1D::SP_mesh mesh = Mesh1D::Create(fm_core, cm_core, mt_core0);
+
+   return mesh;
+  }
 
 }
 
@@ -184,8 +234,8 @@ InputDB::SP_input get_input()
   inp->put<std::string>("eigen_solver",       "arnoldi");
   inp->put<int>("eigen_max_iters",            200);
   inp->put<double>("eigen_tolerance",            1e-7);
-  inp->put<std::string>("bc_west",                    "reflect");
-  inp->put<std::string>("bc_east",                    "reflect");
+  inp->put<std::string>("bc_west",                    "vacuum");
+  inp->put<std::string>("bc_east",                    "vacuum");
   inp->put<int>("quad_number_polar_octant",   16);
 
   return inp;
