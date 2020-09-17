6cad5741740e2dfe9073120a677c1d771eaf85d6
Relzohery <rabab.elzohery@gmail.com>
2020-09-16 21:57:49 -0500

    change to the diffusion transient

--- src/solvers/rom/Kinetic_Mat.hh
+++ src/solvers/rom/Kinetic_Mat.hh
@@ -4,23 +4,23 @@
  *  Created on: Sep 1, 2020
  *      Author: rabab
  */
+#include "kinetics/TimeDependentMaterial.hh"
 #include "utilities/InputDB.hh"
-#include "material/Material.hh"
-#include "kinetics/KineticsMaterial.hh"
 #include "callow/matrix/MatrixBase.hh"
 #include "callow/matrix/MatrixDense.hh"
-
 #include "material/Material.hh"
 #include "geometry/Mesh.hh"
+
+using namespace detran;
 
 class Kinetic_Mat
 {
   public:
-	typedef callow::MatrixDense::SP_matrix             SP_matrix;
-	typedef callow::Vector::SP_vector                 SP_vector;
-	typedef detran_utilities::InputDB::SP_input       SP_input;
-	typedef detran_geometry::Mesh::SP_mesh                SP_mesh;
-	typedef detran::KineticsMaterial::SP_material        SP_material;
+	typedef callow::MatrixDense::SP_matrix                 SP_matrix;
+	typedef callow::Vector::SP_vector                      SP_vector;
+	typedef detran_utilities::InputDB::SP_input            SP_input;
+	typedef detran_geometry::Mesh::SP_mesh                 SP_mesh;
+	typedef TimeDependentMaterial::SP_material             SP_material;
 
 	Kinetic_Mat(SP_input inp, SP_mesh mesh, SP_material mat, SP_matrix basis_f, SP_matrix basis_p);
 	SP_matrix Mat1();
