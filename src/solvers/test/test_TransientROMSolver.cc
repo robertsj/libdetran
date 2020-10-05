1e3059729537f1fba71ef564534af37f63fef53c
Relzohery <rabab.elzohery@gmail.com>
2020-10-05 10:58:40 -0500

    formatting

--- src/solvers/test/test_TransientROMSolver.cc
+++ src/solvers/test/test_TransientROMSolver.cc
@@ -1,12 +1,4 @@
-/*
- * test_TransientROMSolver.cc
- *
- *  Created on: Sep 10, 2020
- *      Author: rabab
- */
-
-#define TEST_LIST               \
-        FUNC(test_TransientSolver)
+#define TEST_LIST                       FUNC(test_TransientSolver)
 
 #include "TestDriver.hh"
 #include "Mesh1D.hh"
@@ -46,101 +38,69 @@ int test_TransientSolver(int argc, char *argv[])
   Mesh1D::SP_mesh mesh = get_mesh(3);
 
   TimeDependentMaterial::SP_material mat(new SlabMaterial('transport'));
+  EigenvalueManager<_1D> manager(inp, mat, mesh);
+  manager.solve();
+  State::SP_state ic = manager.state();
+  mat->set_eigenvalue(ic->eigenvalue());
+  mat->update(0, 0, 1, false);
 
-  //mat->display();
+  vec_int matmap = mesh->mesh_map("MATERIAL");
+
+  // Normalize state.
+  double F = 0;
+  for (int i = 0; i < mesh->number_cells(); ++i)
+  {
+   int m = matmap[i];
+   F += (ic->phi(0)[i]) * mat->sigma_f(m, 0) +
+        (ic->phi(1)[i]) * mat->sigma_f(m, 1);
+  }
+
+  ic->scale(1.0/F);
+
+  printf(" %1.16f", ic->eigenvalue());
+  /*
+  std::cout << "//-----------------  fom --------------------------------//";
+  time_t begin, end;
+  time(&begin);
+  TS_1D stepper(inp, mat, mesh, true);
+  stepper.solve(ic);
+
+  time(&end);
+  time_t elapsed = end - begin;
+
+  State::SP_state final = stepper.state();
+
+  SP_matrix phi_mat;
+  SP_matrix precursors_mat;
+ */
+  //phi_mat = stepper.flux_mat;
+  //precursors_mat = stepper.precursors_mat;
+  //phi_mat->print_matlab("1d_flux.txt");
+  //precursors_mat->print_matlab("1d_precursors.txt");
+
+  std::cout << "//-----------------  rom --------------------------------//";
 
   const char* flux_basis = "/home/rabab/opt/detran/source/src/solvers/test/1D_slab_transient_flux_basis_fm=3";
   const char* precursors_basis = "/home/rabab/opt/detran/source/src/solvers/test/1D_slab_transient_precurses_basis_fm=3";
 
+
+  int r = 10;
+  time_t begin2, end2;
+  time(&begin2);
   SP_matrix basis_f;
-  basis_f = new callow::MatrixDense(42, 20);
+  basis_f = new callow::MatrixDense(42, 2*r);
   ROMBasis::GetBasis(flux_basis, basis_f);
 
   SP_matrix basis_p;
-  basis_p = new callow::MatrixDense(168, 10);
+  basis_p = new callow::MatrixDense(168, r);
   ROMBasis::GetBasis(precursors_basis, basis_p);
 
   //
   TransientSolver R(inp, mesh, mat, basis_f, basis_p);
 
-  //
-  //get initial state
-
-  EigenvalueManager<_1D> manager(inp, mat, mesh);
-  manager.solve();
-  State::SP_state ic = manager.state();
-  mat->set_eigenvalue(ic->eigenvalue());
-  std::cout << " *** updating materials after SS solve ******" << "\n";
-  mat->update(0, 0, 1, false);
-
-  // solve
-  std::cout << " *** Normalize state ******" << "\n";
- // normalize the vector
-  int n = 21;
-
-  callow::Vector phi(42, 0.0);
-  for (int g=0; g<2; g++)
-  {
-   for (int i =0; i< n; i++)
-   {
-    phi[i + g*n] = ic->phi(g)[i];
-   }
-  }
-
-  for (int g=0; g<2; g++)
-    {
-     for (int i =0; i< n; i++)
-     {
-      phi[i + g*n] = ic->phi(g)[i];
-     }
-    }
-
-  double norm  = phi.norm();
-  for (int g=0; g<2; g++)
-    {
-     for (int i =0; i< n; i++)
-     {
-       ic->phi(g)[i] = ic->phi(g)[i]/norm;
-     }
-    }
-
-  std::cout <<"not normed  = " << phi.norm() << "\n";
-
-  std::cout << norm << "\n";
-
-  // Normalize state.
-  double F = 0;
-  phi.scale(1/norm);
-
-  std::cout <<"normed  = " << phi.norm() << "\n";
-
-  vec_int matmap = mesh->mesh_map("MATERIAL");
-  for (int i = 0; i < mesh->number_cells(); ++i)
-  {
-    int m = matmap[i];
-    F += (ic->phi(0)[i]) * mat->sigma_f(m, 0)+
-         (ic->phi(1)[i]) * mat->sigma_f(m, 1);
-  }
-
-  ic->scale(1.0/F);
-  std::cout << ic->eigenvalue() << "\n";
-  printf("eigen value %1.16f " ,ic->eigenvalue()) ;
-
-  std::cout << " ***SOLVE ROM ******" << "\n";
-
   R.Solve(ic);
-
-  std::cout << " ***reconstruct ******" << "\n";
-  std::cout <<  "  ************* END TEST **************************" << "\n";
-
-  R.reconstruct();
-
-  std::cout <<  "  ************* END TEST **************************" << "\n";
+  time(&end2);
+  time_t elapsed2 = end2 - begin2;
 
 return 0;
 }
-
-
-
-
-
