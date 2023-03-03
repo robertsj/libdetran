3d0251db22a2690947214e306d9e6e1aeec968d9
robertsj <robertsj@ksu.edu>
2023-03-03 11:36:56 -0600

    Moved big inline method to source file

--- src/callow/solver/LinearSolver.i.hh
+++ src/callow/solver/LinearSolver.i.hh
@@ -12,24 +12,6 @@
 namespace callow
 {
 
-//----------------------------------------------------------------------------//
-inline int LinearSolver::solve(const Vector &b, Vector &x)
-{
-  Require(x.size() == b.size());
-  Require(x.size() == d_A->number_rows());
-  // Resize the norm
-  d_residual.resize(d_maximum_iterations+1, 0.0);
-  d_status = RUNNING;
-  solve_impl(b, x);
-  if (d_status ==  MAXIT && d_monitor_level > 0)
-  {
-     printf("*** %s did not converge within the maximum number of iterations\n",
-            d_name.c_str());
-  }
-  // Resize the norm
-  d_residual.resize(d_number_iterations+1);
-  return d_status;
-}
 
 } // end namespace callow
 
